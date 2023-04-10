f853b095629d928bd844e7d9cbbaad1a5687ce47
CaSa078 <99073112@mydavinci.nl>
2023-04-10 11:19:46 +0200

    new

--- module_1/voorbeelden/scriptr9.js
+++ module_1/voorbeelden/scriptr9.js
@@ -1,22 +1,60 @@
-console.log("hfuehf")
-let naamIngelogd = "";
-let loggin = []
-function handleLogin(){
-    let naam = document.getElementById('InputNaam').value;
-    console.log("heb je geklikt")
-    if (loggin.includes == (naam)){
-        loggin.splice(loggin.indexOf(naam), 1)
-        console.log("u bent uitgelogd")
-        document.getElementById('melding').innerText = 'U bent uitgelogd: ' + naam;
-        
-    }
-    else {
-        loggin.push(naam);
-        document.getElementById('melding').innerText = 'U bent ingelogd: ' + naam;
-        
-        console.log(loggin)
-    }
-    document.getElementById('InputNaam').value = '';
-    console.log(loggin)
+let loggedIn = [];
+
+function getGewerktString(datumNew , datumOld){
+    let seconden = (datumNew - datumOld) / 1000;
+    let minuten = 0;
+    if(seconden > 60){
+        minuten = Math.floor(seconden/60);
+
+    };
+    seconden -= minuten * 60
+    return 'gewerkt:' + minuten + ' minuten' + seconden + "seconden.";
 }
 
+
+
+
+
+function getIndexOfElementByName(lijst, naam){
+    for(let x = 0; x < lijst.length; x++){
+        if (lijst[x]['naam']=== naam){
+            return x;
+        }
+    }
+    
+    
+    return null;
+}
+
+function handleLogin(){
+    console.log(loggedIn); 
+    console.log(Date())
+   
+    let naam = document.getElementById('inputNaam').value;
+    if (naam.length <= 0){
+        alert("Voer een naam in!")
+        return;
+    }
+    console.log("U hebt op de button geklikt");
+    const positie = getIndexOfElementByName (loggedIn, naam);
+    if (positie != null){ 
+        const oldDate = loggedIn[positie]['moment']
+        const newDate = new Date();
+        const tijd = (oldDate - newDate) / 1000
+        loggedIn.splice(positie, 1); 
+        console.log("U bent uitgelogd");
+        document.getElementById('melding').innerText = 'U bent uitgelogd: ' + naam;
+        
+    } else {
+        loggedIn.push({'naam': naam, 'moment' : new Date()}); 
+        console.log("U bent ingelogd");
+        document.getElementById('melding').innerText = 'U bent ingelogd: ' + naam;
+    }
+    document.getElementById('inputNaam').value = '';
+    console.log(loggedIn);
+    // console.log("U bent ingelogd: " + naam);
+    // document.getElementById('melding').innerText = 'Gelukt';
+    
+}
+
+document.getElementById("inlogButton").onclick = handleLogin;
